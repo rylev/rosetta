369c305a3c4fdb8f3e0cac2ccbb2f3d9bfcda682
Josep M. Bach <josep.m.bach@gmail.com>
2014-03-27 20:27:34 +0100

    Output translated strings

--- main.hs
+++ main.hs
@@ -4,8 +4,10 @@ import Data.List
 import Data.Map (fromListWith, toList)
 import Data.Char (isAlpha)
 
+type TranslatedString = (String, String)
+
 main :: IO ()
-main = print "Hello, world! Sorry for the language"
+main = undefined
 
 tokenize :: String -> [String]
 tokenize = filter (not . null) . (map (filter isAlpha)) . words
@@ -13,4 +15,6 @@ tokenize = filter (not . null) . (map (filter isAlpha)) . words
 frequency :: [String] -> [(String, Integer)]
 frequency = toList . fromListWith (+) . map (\x -> (x, 1))
 
-
+format :: [TranslatedString] -> String
+format translatedStrings = intercalate "\n" $ map (intercalate ": ") stringLists
+  where stringLists = [[x,y] | (x,y) <- translatedStrings]
